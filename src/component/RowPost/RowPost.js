13c7ce9cb2486222a7e40e607b44512c636ad01f
rahulkottikasala <rahulkottikasala@gmail.com>
2022-01-02 02:52:50 +0400

    The banner change with each web refresh

--- src/component/RowPost/RowPost.js
+++ src/component/RowPost/RowPost.js
@@ -1,29 +1,29 @@
-import React, {useEffect, useState} from 'react';
-import {imageUrl} from '../../constants/constants'
-import './RowPost.css'
-import axios from '../../Axios'
+import React, { useEffect, useState } from "react";
+import { imageUrl } from "../../constants/constants";
+import "./RowPost.css";
+import axios from "../../Axios";
 
 const RowPost = (props) => {
-    const [movie, setMovie]  = useState([]);
-    useEffect(() => {
-        axios.get(props.url).then((response) => {
-            setMovie(response.data.results)
-            console.log(response.data.results);
-        })
-    }, [])
-    return (
-        <div className='row'>
-            <h2>{props.title}</h2>
-            <div className="posters">
-                {movie.map((obj) => 
-                    <img className={props.isBig ? 'poster' : 'smallPoster'} src={`${imageUrl+obj.backdrop_path}`} alt="" />
-            )}
-                
-               </div>
+  const [movie, setMovie] = useState([]);
+  useEffect(() => {
+    axios.get(props.url).then((response) => {
+      setMovie(response.data.results);
+      // console.log(response.data.results);
+    });
+  }, []);
+  return (
+    <div className="row">
+      <h2>{props.title}</h2>
+      <div className="posters">
+        {movie.map((obj) => (
+            <img className={props.isBig ? 'poster' : 'smallPoster'}  src={`${imageUrl+obj.backdrop_path}`} alt="" />
+    
+        
+          
+        ))}
+      </div>
+    </div>
+  );
+};
 
-            
-        </div>
-    )
-}
-
-export default RowPost
+export default RowPost;
